0d6faf332bb84b38e2dd268c4abe3bdafcbe5319
Ian Brunjes <ianbrunjes@ucsb.edu>
2021-04-20 11:09:09 -0700

    Updated with functions

--- rockfish_processing.R
+++ rockfish_processing.R
@@ -2,37 +2,8 @@ library(here)
 library(tidyverse)
 library(raster)
 
-# Set our probability threshold for a species to be considered "present"
-presence_threshold = 0.75
 
-# Get list of all the sebastes files
-files <- list.files(path=here("sebastes"), pattern="*.tif", full.names=TRUE, recursive=FALSE)
-
-# Read in cumulative threats raster
-threats_all <- raster(here("full_modelnv.tif"))
-
-#raster_stack <- stack(files)
-#presence_stack <- raster_stack %>% 
-# reclassify(rcl = c(-Inf, 0.75, 0, 0.75,1,1))
-#sebastes_richness <- raster::calc(presence_stack, fun = sum)
-
-# Read in first sebastes raster
-base_raster <- raster(files[1]) %>% reclassify(rcl = c(-Inf, presence_threshold, 0, presence_threshold,1,1))
-
-# Read in each of the other sebastes rasters
-for (i in c(2:length(files))) {
-  # Reclassify as present (1) or not present (0) based on 0.75 probability threshold
-  temp <- raster(files[i]) %>% reclassify(rcl = c(-Inf, presence_threshold, 0, presence_threshold,1,1))
-  # Also set any NAs to 0
-  temp[is.na(temp[])] <- 0 
-  
-  # Add this raster to the cumulative sum of other sebastes presence rasters
-  base_raster <- sum(base_raster, temp)
-}
-
-# base_raster now contains a raster with the number of rockfish species present in each cell
-
-#### Function to output a binary raster based on a user-given quantile (default is top 20%) ###
+## Function to output a binary raster based on a user-given quantile (default is top 20%) ###
 reclassify_topx <- function(rast,quant=0.8) {
   topx <- quantile(rast,quant) #find the 80% quantile of the raster values
   maxVal <- cellStats(rast,max) #find the maximum
@@ -42,41 +13,79 @@ reclassify_topx <- function(rast,quant=0.8) {
   return(out) # returns the new binary raster
 }
 
-# Resample sebastes richness to higher resolution
-base_raster <- resample(base_raster,threats_all,method='ngb',progress='text')
+## Function to output the intersected hotspots raster
+calculate_hotspots <- function(richness_raster, impact_raster, save_with = NULL) {
+  
+  # Get top 20% of richness values
+  richness_raster[richness_raster <= 0] <- NA
+  richness_top_20 <- reclassify_topx(richness_raster, quant=0.8)
+  
+  # Get top 20% of impact values
+  impact_raster[impact_raster <= 0] <- NA
+  impact_top_20 <- reclassify_topx(impact_raster, quant=0.8)
+  
+  # Resample richness to higher resolution
+  richness_top_20 <- resample(richness_top_20,impact_raster,method='ngb',progress='text')
+  
+  # Overlay rasters to get regions that are in both top 20% of impact and top 20% richness
+  hotspots <- overlay(richness_top_20,impact_top_20,fun=function(x,y){x*y})
+  
+  # Set any values of 0 to NA
+  hotspots <- reclassify(hotspots, c(-Inf, 0, NA), right=TRUE)
+  
+  if (!is.null(save_with)) {
+    writeRaster(richness_top_20, paste(save_with,"_richness_20.tif"), overwrite = TRUE)
+    writeRaster(impact_top_20, paste(save_with,"_impact_20.tif"), overwrite = TRUE)
+  }
+  return(hotspots)
+}
 
-# Get top 20% of rockfish richness values
-rockfish_top_20 <- reclassify_topx(base_raster, quant=0.8)
 
-# Get top 20% of impact values
-impact_top_20 <- reclassify_topx(threats_all, quant=0.8)
+### Cumulative impact + total richness Analysis ###
 
-# Overlay rasters to get regions that are in both top 20% of impact and top 20% of rockfish diversity
-hotspots <- overlay(rockfish_top_20,impact_top_20,fun=function(x,y){x*y})
+# Read in cumulative threats raster
+threats_all <- raster(here("full_modelnv.tif"))
 
-# Set any values of 0 to NA
-hotspots <- reclassify(hotspots, c(-Inf, 0, NA), right=TRUE)
+# Read in total species richness data
+total_richness <- raster(here("ca_curr_sp_rich.tif"))
 
-# write files to view in gis
-writeRaster(rockfish_top_20, "sebastes_top20.tif", overwrite = TRUE)
-writeRaster(impact_top_20, "threats_top20.tif", overwrite = TRUE)
-writeRaster(hotspots, "hotspots_cumulative.tif", overwrite = TRUE)
+# Find hotspots
+cumulative_hotspots <- calculate_hotspots(total_richness, threats_all,"cumulative")
+writeRaster(cumulative_hotspots, "hotspots_cumulative.tif", overwrite = TRUE)
 
 
-# Perform analysis for dem destr fishing and recreational fishing threats
+### Sebastes Analysis ###
+
+# Set our probability threshold for a species to be considered "present"
+presence_threshold = 0.75
+
+# Get list of all the sebastes files
+files <- list.files(path=here("sebastes"), pattern="*.tif", full.names=TRUE, recursive=FALSE)
+
+# Read in first sebastes raster
+rockfish_raster <- raster(files[1]) %>% reclassify(rcl = c(-Inf, presence_threshold, 0, presence_threshold,1,1))
+
+# Read in each of the other sebastes rasters
+for (i in c(2:length(files))) {
+  # Reclassify as present (1) or not present (0) based on 0.75 probability threshold
+  temp <- raster(files[i]) %>% reclassify(rcl = c(-Inf, presence_threshold, 0, presence_threshold,1,1))
+  # Also set any NAs to 0
+  temp[is.na(temp[])] <- 0 
+  
+  # Add this raster to the cumulative sum of other sebastes presence rasters
+  rockfish_raster <- sum(rockfish_raster, temp)
+}
+
+#writeRaster(rockfish_raster, "rockfish_richness.tif", overwrite = TRUE)
+
+# Calculate rockfish+dem destr fishing hotspots
 threats_ddf <- raster(here("impact_dem_d.tif"))
-impact_ddf_top_20 <- reclassify_topx(threats_ddf, quant=0.8)
-hotspots_ddf <- overlay(rockfish_top_20,impact_ddf_top_20,fun=function(x,y){x*y})
-hotspots_ddf <- reclassify(hotspots_ddf, c(-Inf, 0, NA), right=TRUE)
+rockfish_ddf_hotspots <- calculate_hotspots(rockfish_raster, threats_ddf)
 writeRaster(hotspots_ddf, "hotspots_dem_des_fish.tif", overwrite = TRUE)
 
+# Calculate rockfish+dem destr fishing hotspots
 threats_rf <- raster(here("impact_rec_fish.tif"))
-impact_rf_top_20 <- reclassify_topx(threats_rf, quant=0.8)
-hotspots_rf <- overlay(rockfish_top_20,impact_rf_top_20,fun=function(x,y){x*y})
-hotspots_rf <- reclassify(hotspots_rf, c(-Inf, 0, NA), right=TRUE)
+rockfish_rf_hotspots <- calculate_hotspots(rockfish_raster, threats_rf)
 writeRaster(hotspots_rf, "hotspots_rec_fish.tif", overwrite = TRUE)
-
-
-
 
 
